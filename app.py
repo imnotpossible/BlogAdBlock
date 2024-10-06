e8c108a5f9661bf79d9b47a2b0ce89c285ce570d
imnotpossible <149683546+imnotpossible@users.noreply.github.com>
2024-10-06 13:29:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ tokenizer = BertTokenizer.from_pretrained('monologg/kobert')
 model = BertForSequenceClassification.from_pretrained('monologg/kobert', num_labels=2)
 #model.load_state_dict(torch.load('C:/Users/jhyun/Desktop/BlogAdBlock/AdBlocK_model.pth', map_location=torch.device('cpu')))
 model.load_state_dict(torch.load('/home/ubuntu/flask/BlogAdBlock/AdBlock_model.pth', map_location=torch.device('cpu')))
+model.to(device)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
